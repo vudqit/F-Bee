01ef139bb14e287cc005eccfddd7627b0e8e0923
Đinh Quang Vũ <vudq96@gmail.com>
2016-11-30 11:22:39 +0700

    Initial Commit

---
+++ F-Bee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  F-Bee
+//
+//  Created by VuDev on 11/30/16.
+//  Copyright © 2016 VuDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
